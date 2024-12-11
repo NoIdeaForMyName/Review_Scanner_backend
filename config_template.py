e1c227a8cfda0bcbe5e15fa3c24cf87b98e4117c
NoIdeaForMyName <maksanty.michal@gmail.com>
2024-12-11 20:06:52 +0100

    refresh token added

--- config_template.py
+++ config_template.py
@@ -12,3 +12,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/mydatabase'  # Replace with actual credentials
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'your_jwt_secret_key'  # Replace with a real secret
+    JWT_TOKEN_LOCATION = ['cookies']
+    JWT_COOKIE_CSRF_PROTECT = True
+    JWT_ACCESS_TOKEN_EXPIRES = 900
+    JWT_REFRESH_TOKEN_EXPIRES = 86400
